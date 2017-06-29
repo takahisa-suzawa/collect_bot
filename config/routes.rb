4ff2416c4542b911cfd9ad97585b3350cc940b9e
takahisa suzawa <t_suzawa@opt.co.jp>
2017-06-29 15:41:26 +0900

    記事を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, { format: 'json' } do
+    resources :articles
+  end
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
