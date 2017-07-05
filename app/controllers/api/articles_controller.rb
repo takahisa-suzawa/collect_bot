9414ce0ce00f3df654d53e744e2d5a74e99efb14
takahisa <t_suzawa@opt.co.jp>
2017-07-05 20:29:16 +0900

    保存を追加

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -27,42 +27,27 @@ module Api
     # POST /articles
     # POST /articles.json
     def create
-        @article = Article.new(article_params)
+        trigger = params[:trigger_word]
+        text = params[:text]
+        timestamp = params[:timestamp]
 
-        respond_to do |format|
-        if @article.save
-            format.html { redirect_to @article, notice: 'Article was successfully created.' }
-            format.json { render :show, status: :created, location: @article }
-        else
-            format.html { render :new }
-            format.json { render json: @article.errors, status: :unprocessable_entity }
-        end
+        
+        begin
+            logger.debug "○○○○○○○○○○○○○○○#{trigger} #{text}️"
+            @article = Article.new(:postedDate => timestamp, :title => trigger)
+            
+            if @article.save
+                render json: @article
+            else
+                format.json { render json: @article.errors, status: :unprocessable_entity }
+            end
+            
+        rescue ActionRecord::RecordInvalid, ActionRecord::RecordNotSaved => ex
+            format.json { render json: ex, status: :unprocessable_entity }
         end
     end
 
-    # PATCH/PUT /articles/1
-    # PATCH/PUT /articles/1.json
-    def update
-        respond_to do |format|
-        if @article.update(article_params)
-            format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-            format.json { render :show, status: :ok, location: @article }
-        else
-            format.html { render :edit }
-            format.json { render json: @article.errors, status: :unprocessable_entity }
-        end
-        end
-    end
-
-    # DELETE /articles/1
-    # DELETE /articles/1.json
-    def destroy
-        @article.destroy
-        respond_to do |format|
-        format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-        format.json { head :no_content }
-        end
-    end
+    
 
     private
         # Use callbacks to share common setup or constraints between actions.
