197106b31a8b6439334dfde7dbfa84c394d70154
takahisa suzawa <t_suzawa@opt.co.jp>
2017-08-03 16:28:00 +0900

    webhookのテスト追加

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -8,22 +8,21 @@ module Api
 
       # POST api/v1/webhooks
       def create
-        @webhook = webhook.new(webhook_params)
-        respond_to do |format|
-          if @webhook.save
-            response = {'text' => "I registered."}
-            format.json response
-          else
-            response = {'text' => "An error occurred."}
-            format.json response
-          end
+        @webhook = Webhook.new(webhook_params)
+        
+        if @webhook.save
+          response = {'text' => "I registered."}
+          render json: response
+        else
+          response = {'text' => "An error occurred."}
+          render json: response
         end
+        
       end
 
       private
-        # Never trust parameters from the scary internet, only allow the white list through.
         def webhook_params
-          params.require(:webhook).permit(:token, :team_id, :team_domain, :channel_id, :channel_name, :timestamp, :user_id, :user_name, :text, :trigger_word)
+          params.permit(:token, :team_id, :team_domain, :channel_id, :channel_name, :timestamp, :user_id, :user_name, :text, :trigger_word)
         end
     end
   end
---
+++ spec/requests/webhooks_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Webhooks", type: :request do
+  describe "GET /api/v1/webhooks" do
+    it "get" do
+      get '/api/v1/webhooks'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /api/v1/webhooks" do
+    before do
+      @params = FactoryGirl.attributes_for(:webhook)
+    end
+    
+    it '200が返ってくる' do
+      post '/api/v1/webhooks', params: @params
+      expect(response).to be_success
+      expect(response.status).to eq 200
+    end
+
+    it 'webhookレコードが1増える' do
+      expect { post '/api/v1/webhooks', params: @params }.to change(Webhook, :count).by(1)
+    end
+  end
+end
