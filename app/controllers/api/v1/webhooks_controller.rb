5a26dec739f12ceaa378f52c4532fb78d570ed88
takahisa suzawa <t_suzawa@opt.co.jp>
2017-08-03 17:19:05 +0900

    url抽出追加

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -12,18 +12,38 @@ module Api
         
         if @webhook.save
           response = {'text' => "I registered."}
-          render json: response
         else
           response = {'text' => "An error occurred."}
-          render json: response
+          render json: response and return
         end
-        
+
+        # webhookの命令を実行する
+        # @セキュリから始まっているか ＜未実装＞
+
+        # URLから始まっているか
+        url = slice_url @webhook.text
+
+        # それ以外はスルーする
+        render json: response
       end
 
       private
-        def webhook_params
-          params.permit(:token, :team_id, :team_domain, :channel_id, :channel_name, :timestamp, :user_id, :user_name, :text, :trigger_word)
+      def webhook_params
+        params.permit(:token, :team_id, :team_domain, :channel_id, :channel_name, :timestamp, :user_id, :user_name, :text, :trigger_word)
+      end
+      
+      def 
+
+      def slice_url(url)
+        url = nil
+        if text =~ /^(<http?|<ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)(>)$/
+            url = $1 + $2 + $3
+        end 
+        if url.nil? && text =~ /^(<https?|<ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)(>)$/
+            url = $1 + $2 + $3
         end
+        return url
+      end
     end
   end
 end
